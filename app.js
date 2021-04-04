d9a9f5b7ee447a089ca68c3ec3c3e7ce9eca6748
bahar <baharhek@yahoo.com>
2021-04-04 18:18:01 +0430

    not

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+// let array = ["alireza" ,"ayda" ,"fatemeh" , "hanieh" ,"bahar"];
+
+// console.log(array.indexOf("ayda"));
+
+let array = ["benz", "bugatti", "ford", "lambo", "BMW", "audi"];
+array.pop();
+array.pop();
+array.pop();
+array.pop();
+array.pop();
+array.pop();
+array.push("peykan");
+array.push("bugatti");
+array.push("ford");
+array.push("l90");
+array.push("rd");
+array.push("pride");
+console.log(array);
